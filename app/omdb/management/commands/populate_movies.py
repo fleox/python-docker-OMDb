0f0e5e74b992d3f6ae6c7c43de7d5a759afbe229
fleox <frederic.leaux@gmail.com>
2023-09-15 10:42:59 +0200

    update command pop movies

--- app/omdb/management/commands/populate_movies.py
+++ app/omdb/management/commands/populate_movies.py
@@ -18,42 +18,43 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
 
         apiKey = "98b330ef"
+        movieslist = ["Pirates of the Caribbean", "Fast & Furious", "Star-Wars"]
+        for movieToFind in movieslist:
+            print(movieToFind)
 
-        #Fetch Movie Data with Full Plot 
-        data_URL = 'http://www.omdbapi.com/?apikey='+apiKey
-        year = ''
-        movie = 'Fast & Furious' 
-        params = {
-            #'i':'tt1905041',
-            's':movie,
-            'type':'movie',
-            'y':year,
-            'plot':'full'
-        }
-        
-        response = requests.get(data_URL,params=params).json()
-
-        k = {}
-        for movie_item in response['Search']:
-            
             #Fetch Movie Data with Full Plot 
             data_URL = 'http://www.omdbapi.com/?apikey='+apiKey
+            year = '' 
             params = {
-                'i':movie_item.get('imdbID'),
+                's':movieToFind,
+                'type':'movie',
+                'y':year,
+                'plot':'full'
             }
-            movie = requests.get(data_URL,params=params).json()
-            #pp.pprint(movie)
             
-            k['name'] = movie.get('Title')
-            k['year'] = movie.get('Year')
-            k['poster'] = movie.get('Poster')
-            k['director'] = movie.get('Director')
-            actors_list = movie.get('Actors').split(", ")
-            movie, created = Movie.objects.get_or_create(**k)
+            response = requests.get(data_URL,params=params).json()
 
-            for name in actors_list:
-                actor, created = Actor.objects.get_or_create(name=name)
-                movie.actor.add(actor)
+            k = {}
+            for movie_item in response['Search']:
+                
+                #Fetch Movie Data with Full Plot 
+                data_URL = 'http://www.omdbapi.com/?apikey='+apiKey
+                params = {
+                    'i':movie_item.get('imdbID'),
+                }
+                movie = requests.get(data_URL,params=params).json()
+                #pp.pprint(movie)
+                
+                k['name'] = movie.get('Title')
+                k['year'] = movie.get('Year')
+                k['poster'] = movie.get('Poster')
+                k['director'] = movie.get('Director')
+                actors_list = movie.get('Actors').split(", ")
+                movie, created = Movie.objects.get_or_create(**k)
 
-            movie.save()
-            print(movie)
+                for name in actors_list:
+                    actor, created = Actor.objects.get_or_create(name=name)
+                    movie.actor.add(actor)
+
+                movie.save()
+                print(movie)
